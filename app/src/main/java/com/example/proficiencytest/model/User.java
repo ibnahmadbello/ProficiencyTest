28ff17562ca36f52e4ee8bf65d1d7e2f75d487bc
ibnahmad <justforlearningcode@gmail.com>
2019-07-09 03:22:09 +0100

    Added model class

---
+++ app/src/main/java/com/example/proficiencytest/model/User.java
@@ -0,0 +1,95 @@
+
+package com.example.proficiencytest.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+public class User {
+
+    @SerializedName("id")
+    @Expose
+    private Long id;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("first_name")
+    @Expose
+    private String firstName;
+    @SerializedName("last_name")
+    @Expose
+    private String lastName;
+    @SerializedName("avatar")
+    @Expose
+    private String avatar;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public User() {
+    }
+
+    /**
+     * 
+     * @param id
+     * @param lastName
+     * @param email
+     * @param avatar
+     * @param firstName
+     */
+    public User(Long id, String email, String firstName, String lastName, String avatar) {
+        super();
+        this.id = id;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.avatar = avatar;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("id", id).append("email", email).append("firstName", firstName).append("lastName", lastName).append("avatar", avatar).toString();
+    }
+
+}
---
+++ app/src/main/java/com/example/proficiencytest/model/UserResult.java
@@ -0,0 +1,96 @@
+
+package com.example.proficiencytest.model;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+public class UserResult {
+
+    @SerializedName("page")
+    @Expose
+    private Long page;
+    @SerializedName("per_page")
+    @Expose
+    private Long perPage;
+    @SerializedName("total")
+    @Expose
+    private Long total;
+    @SerializedName("total_pages")
+    @Expose
+    private Long totalPages;
+    @SerializedName("data")
+    @Expose
+    private List<User> data = null;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public UserResult() {
+    }
+
+    /**
+     * 
+     * @param total
+     * @param page
+     * @param data
+     * @param perPage
+     * @param totalPages
+     */
+    public UserResult(Long page, Long perPage, Long total, Long totalPages, List<User> data) {
+        super();
+        this.page = page;
+        this.perPage = perPage;
+        this.total = total;
+        this.totalPages = totalPages;
+        this.data = data;
+    }
+
+    public Long getPage() {
+        return page;
+    }
+
+    public void setPage(Long page) {
+        this.page = page;
+    }
+
+    public Long getPerPage() {
+        return perPage;
+    }
+
+    public void setPerPage(Long perPage) {
+        this.perPage = perPage;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public Long getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Long totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public List<User> getData() {
+        return data;
+    }
+
+    public void setData(List<User> data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("page", page).append("perPage", perPage).append("total", total).append("totalPages", totalPages).append("data", data).toString();
+    }
+
+}
